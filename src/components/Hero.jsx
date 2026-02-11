c405e05073122a4454a8ae65be70a04104cd1966
Sairaj <saisabale28@gmail.com>
2026-02-11 19:30:52 +0530

    Created Hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+function Hero() {
+  return (
+    <section>
+      <h1>Sairaj Sabale</h1>
+      <p>Computer Engineering Student</p>
+      <p>I build modern web applications using React.</p>
+    </section>
+  )
+}
+
+export default Hero
