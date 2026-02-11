8e93b0b04e47d41e97385d28c63a38ebe97010bb
Sairaj <saisabale28@gmail.com>
2026-02-11 19:35:20 +0530

    Created Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>Sairaj</h2>
+      <ul>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
