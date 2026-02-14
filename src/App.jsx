e785499f48fbcb745f12ec62ac5569dfb1a022fc
Sairaj <saisabale28@gmail.com>
2026-02-14 21:33:28 +0530

    fixed contact import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
-import Contact from "./components/Contact";
+import Contact from "./components/contact";
 
 function App() {
   return (
